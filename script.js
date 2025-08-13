acc78258c60d364495cb4369aad53b67e2698e52
mahammadaftab <mdaftabeditz360@gmail.com>
2025-08-13 23:39:14 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,27 @@
+const downloadBtn = document.querySelector(".download-btn");
+const fileLink = "file:///D:/WEB%20PROJECT/New/Download%20Button%20with%20Timer/Download%20Button%20with%20Timer/MD_AFTAB_360.pdf";
+
+const initTimer = () => {
+    if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
+    }
+    let timer = downloadBtn.dataset.timer;
+    downloadBtn.classList.add("timer");
+    downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+    const initCounter = setInterval(() => {
+        if(timer > 0) {
+            timer--;
+            return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+        }
+        clearInterval(initCounter);
+        location.href = fileLink;
+        downloadBtn.innerText = "Your file is downloading...";
+        setTimeout(() => {
+            downloadBtn.classList.replace("timer", "disable-timer");
+            downloadBtn.innerHTML = `<span class="icon material-symbols-rounded">vertical_align_bottom</span>
+                                     <span class="text">Download Again</span>`;
+        }, 3000);
+    }, 1000);
+}
+
+downloadBtn.addEventListener("click", initTimer);
